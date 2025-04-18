9fc0525a6fa568682528dfcc8f82173db21f9a9b
honehone12 <54838229+honehone12@users.noreply.github.com>
2025-04-18 17:19:59 +0900

    fixed layout

--- src/routes/report/+page.svelte
+++ src/routes/report/+page.svelte
@@ -1,20 +1,24 @@
 <script>
     'use strict';
 
+    import Loading from "$lib/components/Loading.svelte";
+
     async function load() {
 
     }
 </script>
 
-<div class="hero min-h-screen">
-    <div class="hero-content text-center">
-        <div class="p-20">
-            {#await load()}
-                
-            {:then calls} 
-                
-            {/await}
+{#await load()}
+    <div class="hero min-h-screen">
+        <div class="hero-content text-center">
+            <div class="p-20">
+                <Loading/>
+            </div>
         </div>
-    </div>
-</div>
+    </div>                
+{:then reports} 
+                
+{/await}
+
+
 
