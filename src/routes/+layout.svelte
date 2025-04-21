64cc9a8520828232c739332468bbab29191d6302
honehone12 <54838229+honehone12@users.noreply.github.com>
2025-04-21 18:36:59 +0900

    added navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,12 @@
     'use strict';
 
     import "../app.css"
+    import Navbar from "$lib/components/Navbar.svelte";
+    
     let {children} = $props();
 </script>
 
+<Navbar/>
 <main class="w-full">
     {@render children()}
 </main>
