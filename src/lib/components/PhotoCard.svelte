574f61c0fe71d66f794f5b23425e35d1c9fd1b12
honehone12 <54838229+honehone12@users.noreply.github.com>
2025-04-21 22:23:33 +0900

    added download

---
+++ src/lib/components/PhotoCard.svelte
@@ -0,0 +1,22 @@
+<script>
+    'use strict';
+
+    /**
+     * @type {{url: string, name: string}}
+     */
+    let {url, name} = $props();
+</script>
+
+<div class="card card-side bg-base-300 mb-5">
+    <figure>
+        <img src="{url}" class="h-120" alt="{name}"/>
+    </figure>
+    <div class="card-body place-content-center px-10">
+        <h2 class="card-title text-2xl mb-10">{name}</h2>
+        <div class="card-actions justify-center">
+            <a href="{url}" download>
+                <button class="btn btn-primary">ダウンロード</button>
+            </a>
+        </div>
+    </div>
+</div>
--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -1,3 +1,50 @@
 <script>
     'use strict';
+
+    import { goto } from "$app/navigation";
+    import { invoke } from "@tauri-apps/api/core";
+    import Loading from '$lib/components/Loading.svelte';
+    import PhotoCard from "$lib/components/PhotoCard.svelte";
+
+    async function load() {
+        try {
+            /**
+             * @type {import("$lib/api").photos}
+             */
+            const photos = await invoke('load_download');
+            return photos;
+        } catch {
+            goto('/error');
+        }
+
+        return {
+            morning_alc: '',
+            evening_alc: '',
+            meter: ''
+        };
+    }
 </script>
+
+<div class="hero min-h-screen">
+    <div class="hero-content text-center">
+        <div class="p-20">
+                {#await load()}
+                <Loading/>
+                {:then photos}
+                    <div class="mb-15">
+                        <p>リンクは30分間有効です</p>
+                        <p>リンクが切れた場合は、一度他の日付を閲覧してから再試行して下さい</p>
+                    </div> 
+                    {#if photos.morning_alc}
+                        <PhotoCard url="{photos.morning_alc}" name="前点呼　アルコール"/>
+                    {/if}
+                    {#if photos.evening_alc}
+                        <PhotoCard url="{photos.evening_alc}" name="後点呼　アルコール"/>
+                    {/if}
+                    {#if photos.meter}
+                        <PhotoCard url="{photos.meter}" name="日報　メーター"/>
+                    {/if}
+                {/await}
+        </div>
+    </div>
+</div>                
