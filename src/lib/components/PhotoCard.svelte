669bb39a22b0b2ea5bbe003b5365b62a47349f73
honehone12 <54838229+honehone12@users.noreply.github.com>
2025-05-16 17:47:30 +0900

    fixed layout

--- src/lib/components/PhotoCard.svelte
+++ src/lib/components/PhotoCard.svelte
@@ -9,12 +9,14 @@
 
 <div class="card card-side bg-base-200">
     <figure>
-        <img src="{url}" class="w-120" alt="{name}"/>
+        <img src="{url}" class="w-100" alt="{name}"/>
     </figure>
-    <div class="card-body place-content-center text-center px-10">
-        <h2 class="card-title text-xl mb-5">{name}</h2>
-        <div class="card-actions justify-center">
-            <a href="{url}" download>
+    <div class="card-body place-content-center px-10">
+        <div class="text-center">
+            <h2 class="text-xl mb-5">{name}</h2>
+        </div>
+        <div class="justify-center">
+            <a href="{url}">
                 <button class="btn btn-primary">ダウンロード</button>
             </a>
         </div>
